9ab8a581da53cadbecca62be5cfbcb805bdf5e13
Thao Nguyen <thaonguyen@wifi185-157.bucknell.edu>
2025-04-10 15:19:26 -0400

    trying to extract text

--- extract_text.py
+++ extract_text.py
@@ -1,8 +1,7 @@
-import fitz  # PyMuPDF
+import pymupdf
 
-doc = fitz.open("pdfs/b_1960-1961 pg 1 sample.pdf")
+doc = pymupdf.open("pdfs/b_1960-1961 pg 1 sample.pdf")
 for page in doc:
     text = page.get_text()
     print(text)
-    
 doc.close()
