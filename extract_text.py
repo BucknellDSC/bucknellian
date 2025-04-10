e9435b90f663c154fb71c7fb539406518679a33b
Thao Nguyen <thaonguyen@wifi185-157.bucknell.edu>
2025-04-10 17:49:58 -0400

    fixed for 1 page

--- extract_text.py
+++ extract_text.py
@@ -1,8 +1,24 @@
-import fitz  # "top-level Python import name of the PyMuPDF library"
+import fitz  # PyMuPDF library
 
+# Path to the PDF file
 ex_path = "pdfs/b_1960-1961 pg 1 sample.pdf"
+
+#output_text.txt
 doc = fitz.open(ex_path)
 for page in doc:
     text = page.get_text()
     print(text)
 doc.close()
+
+# # Open the PDF document
+# try:
+#     doc = fitz.open(ex_path)
+#     for page_num, page in enumerate(doc, start=1):
+#         # Extract text from each page
+#         text = page.get_text()
+#         print(f"--- Page {page_num} ---")
+#         print(text)
+#         print("\n")
+# finally:
+#     # Ensure the document is closed properly
+#     doc.close()
