b9b712019ffb04a96ab9613badcab2f883aff109
Thao Nguyen <thaonguyen@wifi185-157.bucknell.edu>
2025-04-10 17:38:13 -0400

    organizing txt files

--- extract_text.py
+++ extract_text.py
@@ -1,6 +1,7 @@
-import pymupdf
+import fitz  # "top-level Python import name of the PyMuPDF library"
 
-doc = pymupdf.open("pdfs/b_1960-1961 pg 1 sample.pdf")
+ex_path = "pdfs/b_1960-1961 pg 1 sample.pdf"
+doc = fitz.open(ex_path)
 for page in doc:
     text = page.get_text()
     print(text)
